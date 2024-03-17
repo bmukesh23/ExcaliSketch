bf96e777aea86cc8fe10e700c55be61a6b7ee1e0
Mukesh <billamukesh1234@gmail.com>
2024-03-17 17:10:26 +0530

    feat: implementing rough.js for canvas drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-function App() {
+import { useLayoutEffect } from "react";
+import rough from 'roughjs';
+
+export default function App() {
+  useLayoutEffect(() => {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    const roughCanvas = rough.canvas(canvas);
+    roughCanvas.rectangle(10, 10, 200, 200);
+  });
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <canvas id="canvas" width={innerWidth} height={innerHeight}></canvas>
+    </div>
   )
 }
-
-export default App
