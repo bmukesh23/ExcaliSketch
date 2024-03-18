d176badb83a2233d4f5e8592d14ea16beb54e9e4
Mukesh <billamukesh1234@gmail.com>
2024-03-18 22:55:14 +0530

    add: clear button

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,8 @@ export default function App() {
 
   return (
     <div>
-      <div style={{ position: 'fixed' }}>
+      <div style={{ position: 'fixed'}}>
+        <button onClick={() => setElements([])}>Clear</button>
         <input
           type="radio"
           name="line"
